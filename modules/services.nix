6039c37e4c22976d73b307ca4ee39704a3b787b5
Chloe Amanita <dollyabsurdism@gmail.com>
2025-11-08 18:56:06 -0300

    yayy

--- modules/services.nix
+++ modules/services.nix
@@ -9,10 +9,16 @@
   #enable bluetooth manager
   services.blueman.enable = true;
 
-  # enable flatpak
-  services.flatpak.enable = true;
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-  # enable bluetooth manager
-  services.blueman.enable = true;
-
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 }
