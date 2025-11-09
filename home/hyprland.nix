ce5723173d71a2d23bd106c9548abd04e1cf28d5
Chloe Amanita Nomad <dollyabsurdism@gmail.com>
2025-11-09 19:03:51 -0300

    ricin...!!!!AAAAAAAA

---
+++ home/hyprland.nix
@@ -0,0 +1,15 @@
+{config, pkgs, inputs, ...} :
+
+{
+
+  wayland.windowManager.hyprland = {
+    # Whether to enable Hyprland wayland compositor
+    enable = true;
+    # The hyprland package to use
+    package = pkgs.hyprland;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+
+ };
+
+}
