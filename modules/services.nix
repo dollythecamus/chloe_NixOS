4268840b30a5f326ac445ad1d5c93c7140727d13
Chloe Amanita <dollyabsurdism@gmail.com>
2026-05-02 18:49:53 -0300

    yye

--- modules/services.nix
+++ modules/services.nix
@@ -19,6 +19,11 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  #enable docker
+  virtualisation.docker = {
+    enable = true;
+  };
+
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
