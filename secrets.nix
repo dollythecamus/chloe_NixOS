a9ca279964727d9bf0fde4b0e088b2b0c4877280
Chloe Amanita <dollyabsurdism@gmail.com>
2026-06-05 15:51:28 -0300

    secrets updated

--- secrets.nix
+++ secrets.nix
@@ -11,14 +11,21 @@
     "/etc/ssh/ssh_host_ed25519_key"
     ];
 
-  age.secrets.secret1.file = ./secrets/secret1.age;
+  #age.secrets.auth_secret1.file = ./secrets/auth_secret1.age;
 
   age.secrets.openclaw_gateway_token.file = ./secrets/openclaw/gateway_token.age;
   age.secrets.discord_bot_token.file = ./secrets/openclaw/discord_token.age;
 
+  # shell at startup to define the gateway token by getting the secret
+  environment.shellInit = ''
+    if [ -f ${config.age.secrets.openclaw_gateway_token.path} ]; then
+      export OPENCLAW_GATEWAY_TOKEN=$(cat ${config.age.secrets.openclaw_gateway_token.path})
+      export OPENCLAW_DISCORDBOT_TOKEN=$(cat ${config.age.secrets.discord_bot_token.path})
+      
+    fi
+  '';
+
   environment.sessionVariables = {
-    OPENCLAW_GATEWAY_TOKEN = config.age.secrets.openclaw_gateway_token.path;
-    OPENCLAW_DISCORD_BOT_TOKEN = config.age.secrets.discord_bot_token.path;
   };
   
 }
