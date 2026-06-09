ed67f27fa9681dbfbae4e8eda7fd5e051adac53e
Chloe Amanita <dollyabsurdism@gmail.com>
2026-06-09 15:16:44 -0300

    removed shellInit from secrets

--- secrets.nix
+++ secrets.nix
@@ -16,16 +16,9 @@
   age.secrets.openclaw_gateway_token.file = ./secrets/openclaw/gateway_token.age;
   age.secrets.discord_bot_token.file = ./secrets/openclaw/discord_token.age;
 
-  # shell at startup to define the gateway token by getting the secret
-  environment.shellInit = ''
-    if [ -f ${config.age.secrets.openclaw_gateway_token.path} ]; then
-      export OPENCLAW_GATEWAY_TOKEN=$(cat ${config.age.secrets.openclaw_gateway_token.path})
-      export OPENCLAW_DISCORDBOT_TOKEN=$(cat ${config.age.secrets.discord_bot_token.path})
-      
-    fi
-  '';
-
   environment.sessionVariables = {
+    OPENCLAW_GATEWAY_TOKEN="${config.age.secrets.openclaw_gateway_token.path}";
+    OPENCLAW_DISCORDBOT_TOKEN="${config.age.secrets.discord_bot_token.path}";
   };
   
 }
